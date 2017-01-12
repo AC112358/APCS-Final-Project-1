fe350d4d3d6c559b1d19c295cbc97da04009b567
Ann Caplin <ann.caplin@gmail.com>
2017-01-12 17:22:22 -0500

    Update Utilities (time convert)

--- src/Utilities.java
+++ src/Utilities.java
@@ -2,28 +2,41 @@ public class Utilities{
     public static String makeMilitary(String time){
 	//time is formated hh:mm AM/PM
 	time = time.toUpperCase();
-	time = String.join(time.split(" "));
+	String[] timeArr = time.split(" ");
+	time = "";
+	for (int i = 0; i < timeArr.length; i++){
+	    time += timeArr[i];
+	}
 	String h = "";
 	String m = "";
 	String amPm = "";
 	try{
-	    String timeStrings = time.split(":");
+	    //  System.out.println(time);
+	    String[] timeStrings = time.split(":");
+	    //   System.out.println(timeStrings[0] + " " +timeStrings[1]);
 	    h = timeStrings[0];
 	    double hour = Double.parseDouble(h);
-	    m = timeStrings[0].substring(0, 2);
+	    m = timeStrings[1].substring(0, 2);
 	    double minute = Double.parseDouble(m);
-	    amPm = timeStrings[0].substring(2);
+	    // System.out.println(hour +  " " + minute);
+	    amPm = timeStrings[1].substring(2);
 	    if (amPm.equals("AM")){
 		if (hour == 12){
 		    return "00" + m;
 		}
-		return h + m;
+		if (hour < 12){
+		    return h + m;
+		}
+		return "ERROR: Time invalid";
 	    }
 	    else if(amPm.equals("PM")){
 		if (hour == 12){
 		    return h + m;
 		}
-		return "" + (hour+12) + m;
+		if (hour < 12){
+		    return "" + (int)(hour+12) + m;
+		}
+		return "ERROR: Time invalid";
 	    }
 	    else{
 		return "ERROR: Time invalid";
@@ -33,7 +46,7 @@ public class Utilities{
 	    return "ERROR: Time invalid";
 	}
     }
-    public static void main(String[] args){
-	System.out.println(makeMilitary("12: 33 PM"));
-    }
+    /* public static void main(String[] args){
+	System.out.println(makeMilitary("5:34 PM"));
+	}*/
 }
