e93bc71232977507b7248e5e27312b1b8b8d99bf
QuillChen50 <jchen50@stuy.edu>
2017-01-13 12:01:05 -0500

    found problem

--- GuiRetry/TabPanes/TabPanes.java
+++ GuiRetry/TabPanes/TabPanes.java
@@ -3,14 +3,15 @@ import java.awt.*;
 
 public class TabPanes extends JPanel
 {
-        JFrame window = new JFrame();
-    	JTabbedPane spi = new JTabbedPane();
-	JScrollPane t = new JScrollPane();
-	JScrollPane b = new JScrollPane();
-	JSplitPane p = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-				      t,b);
+    //JFrame window = new JFrame();
+    JPanel window = new JPanel();
+    JTabbedPane spi = new JTabbedPane();
+    JScrollPane t = new JScrollPane();
+    JScrollPane b = new JScrollPane();
+    JSplitPane p = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+				  t,b);
 
-    //    public TabPanes() {
+    public TabPanes() {
 	//splitpane settings
 	p.setOneTouchExpandable(true);
         p.setDividerLocation(750);
@@ -29,45 +30,45 @@ public class TabPanes extends JPanel
 	t.add(st);
 	b.add(sb);
 	//what didn't work
-	getContentPane().add(getTabbedPane(), BorderLayout.CENTER);
+	window.add(p, BorderLayout.CENTER);
 	//JFrame.setContentPane(JTabbedPane spi);
 
     }
 
-	//gets
-	public JTabbedPane getTabbedPane()
-	{
-	    return spi;
-        }
+    //gets
+    public JTabbedPane getTabbedPane()
+    {
+	return spi;
+    }
     
 	
 
-	public static void createAndShowGui (){
+    public static void createAndShowGui (){
 
-	    JFrame f = new JFrame("Schedule Planner Doc");
-	    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    f.setSize(1600,900);
-	    TabPanes tp = new TabPanes();
-	    f.getContentPane().add(tp.getTabbedPane());
+	JFrame f = new JFrame("Schedule Planner Doc");
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	f.setSize(1600,900);
+	TabPanes tp = new TabPanes();
+	f.getContentPane().add(tp.getTabbedPane());
 
-	    //Display
+	//Display
 	   
-	    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-	    int xcor = (int) ((d.getWidth() - f.getWidth()) / 2);
-	    int ycor = (int) ((d.getHeight() - f.getHeight()) / 2);
-	    f.setLocation(xcor, ycor);
+	Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+	int xcor = (int) ((d.getWidth() - f.getWidth()) / 2);
+	int ycor = (int) ((d.getHeight() - f.getHeight()) / 2);
+	f.setLocation(xcor, ycor);
 
-	    f.pack();
-	    f.setVisible(true);
-	}
-
-	public static void main (String[] args){
-	       javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	       public void run() {
-                createAndShowGui();
-	    }
-	    });
-	    }
-
+	f.pack();
+	f.setVisible(true);
     }
+
+    public static void main (String[] args){
+	javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		public void run() {
+		    createAndShowGui();
+		}
+	    });
+    }
+    
+}
 	
